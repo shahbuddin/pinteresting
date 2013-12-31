80f4dce169b1038760be6542e28f56eb6c9bfb97
Sarosh Shahbuddin <sarosh.shahbuddin@gmail.com>
2013-12-31 02:09:36 -0500

    Add user name, location, and team to application_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131229065459) do
+ActiveRecord::Schema.define(version: 20131231065054) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20131229065459) do
     t.datetime "updated_at"
     t.string   "user_location"
     t.string   "user_team"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
